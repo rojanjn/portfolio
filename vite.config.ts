e7130027348645cba464f39ab696f1d909b81aa8
Rojan Jafarnezhad <rojanjafarnezhad@gmail.com>
2026-01-24 18:02:11 -0500

    Add initial HTML structure for Rojan's Portfolio

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@
 
   export default defineConfig({
     plugins: [react()],
-    base: '/portfolio/portfolio',
+    base: '/portfolio/',
     resolve: {
       extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
       alias: {
@@ -52,7 +52,7 @@
     },
     build: {
       target: 'esnext',
-      outDir: 'build',
+      outDir: 'docs',
     },
     server: {
       port: 3000,
