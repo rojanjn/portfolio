8fc934c74538309441b58c8a6c3559a569255db5
Rojan Jafarnezhad <rojanjafarnezhad@gmail.com>
2026-01-24 17:59:16 -0500

    Update asset paths in index.html and adjust Vite base path for deployment consistency

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@
 
   export default defineConfig({
     plugins: [react()],
-    base: '/portfolio/',
+    base: '/portfolio/portfolio',
     resolve: {
       extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
       alias: {
